f8f01a292d55b4e86de7605f55923352639e6962
Pcjmy <1716045191@qq.com>
2023-01-25 19:26:10 +0800

    配置ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,34 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        endOfLine: 'auto', //不让prettier检测文件每行结束的格式
+      },
+    ],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
       },
     },
   ],
-};
+}
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## 技术栈
 
 Vue3 + TS
+
+## 安装工具
+
+```shell
+npm i eslint prettier-eslint eslint-config-prettier --save-dev
+npm install @vue/eslint-config-prettier
+```
