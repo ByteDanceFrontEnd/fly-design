6e65dfefecb9b591cb5bd2bcea223cb6146c73a7
zgq <755609767@qq.com>
2023-02-07 10:41:52 +0800

    style: 默认主题色

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,19 +1,35 @@
+// 引入默认主题
 import DefaultTheme from 'vitepress/theme'
+// 引入进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+NProgress.configure({ showSpinner: true })
+// 引入组件库组件
+import globals from '../client'
+// 引入elementplus
 // import "element-plus/dist/index.css";
 // import ElementPlus from "element-plus";
-import globals from '../client'
+import { useRouter } from 'vitepress'
+import { watch, nextTick } from 'vue'
+import './style.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp: async ({ app }) => {
-    // app is the Vue 3 app instance from `createApp()`. router is VitePress'
-    // custom router. `siteData`` is a `ref`` of current site-level metadata.
-    // import("element-plus").then((module) => {
-    //   app.use(module);
-    // });
     // app.use(ElementPlus);
+    // 注册组件库组件
     globals.forEach(([name, Comp]) => {
       app.component(name, Comp)
+    })
+  },
+  // 进度条
+  setup() {
+    const router = useRouter()
+    watch(router.route, (newVal, oldVal) => {
+      NProgress.start()
+      nextTick(() => {
+        NProgress.done()
+      })
     })
   },
 }
