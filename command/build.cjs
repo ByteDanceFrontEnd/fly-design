9f928315d751688f8ea6ea8126c316d11aa7e301
Pcjmy <1716045191@qq.com>
2023-02-13 21:54:59 +0800

    feat: 单独打包每个组件

--- command/build.cjs
+++ command/build.cjs
@@ -2,6 +2,8 @@ const path = require('path')
 const { defineConfig, build } = require('vite')
 const vue = require('@vitejs/plugin-vue')
 const vueJsx = require('@vitejs/plugin-vue-jsx')
+const fsExtra = require('fs-extra')
+const fs = require('fs')
 
 // 打包入口文件
 const entryDir = path.resolve(__dirname, '../packages')
@@ -44,9 +46,61 @@ const buildAll = async () => {
   )
 }
 
+// 单组件打包
+const buildSingle = async (name) => {
+  await build(
+    defineConfig({
+      ...baseConfig,
+      build: {
+        rollupOptions,
+        lib: {
+          entry: path.resolve(entryDir, name),
+          name: 'index',
+          fileName: 'index',
+          formats: ['es', 'umd'],
+        },
+        outDir: path.resolve(outputDir, name),
+      },
+    }),
+  )
+}
+
+// 每个组件生成package.json
+const createPackageJson = (name) => {
+  const fileStr = `
+  {
+    "name": "${name}",
+    "main": "index.umd.js",
+    "module": "index.mjs",
+    "style": "style.css"
+  }
+`
+
+  fsExtra.outputFile(
+    path.resolve(outputDir, `${name}/package.json`),
+    fileStr,
+    'utf-8',
+  )
+}
+
 // 打包成库
 const buildLib = async () => {
   await buildAll()
+  // 获取组件名称组成的数组
+  const components = fs.readdirSync(entryDir).filter((name) => {
+    const componentDir = path.resolve(entryDir, name)
+    const isDir = fs.lstatSync(componentDir).isDirectory()
+    return isDir && fs.readdirSync(componentDir).includes('index.ts')
+  })
+
+  // 循环构建
+  for (const name of components) {
+    // 构建单组件
+    await buildSingle(name)
+
+    // 生成组件的 package.json 文件
+    createPackageJson(name)
+  }
 }
 
 buildLib()
