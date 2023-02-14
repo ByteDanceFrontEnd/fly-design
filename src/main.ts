167febbe4fe8ad445edd839ac46baa95ef0210a7
Pcjmy <1716045191@qq.com>
2023-02-14 15:35:34 +0800

    feat: 发布到npm官网

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import flyd from '../lib/fly-design.js'
-// import '../lib/style.css'
-import Input from '../lib/Input/index.js'
-import '../lib/Input/style.css'
+import flyd from '../lib/fly-design.js'
+import '../lib/style.css'
+// import Input from '../lib/Input/index.js'
+// import '../lib/Input/style.css'
 const app = createApp(App)
 
-// app.use(flyd)
-app.use(Input)
+app.use(flyd)
+// app.use(Input)
 app.mount('#app')
